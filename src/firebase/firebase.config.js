f738284f71b9a140b638c83472a4deda1970fc2b
mainur rahaman <enan.mainur@gmail.com>
2021-11-27 06:03:54 +0600

    firebase edited

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAcuegs_82wAeyExG9iRY7se5tuYKDdx9E",
-    authDomain: "jibon-firebase-auth.firebaseapp.com",
-    projectId: "jibon-firebase-auth",
-    storageBucket: "jibon-firebase-auth.appspot.com",
-    messagingSenderId: "18169683239",
-    appId: "1:18169683239:web:12cca658c7bcf684da9d02"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
 export default firebaseConfig;
